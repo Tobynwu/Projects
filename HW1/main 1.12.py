22012900f4b5174e1031f682d2b528041299a1ec
Tobynwu <90466819+Tobynwu@users.noreply.github.com>
2023-02-15 03:04:00 -0600

    Add files via upload

---
+++ HW1/main 1.12.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Tobenna Nwufo
+2054054
+CISC 2348
+"""
+userNum = int(input())
+userNumSquared = userNum ** 2# Bug here; fix it when instructed
+
+print(userNumSquared)# Output formatting issue here; fix it when instructed
