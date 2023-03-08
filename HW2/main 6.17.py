70c7a572f5b6f70c69567644d44541f3f159903e
Tobynwu <90466819+Tobynwu@users.noreply.github.com>
2023-03-08 02:32:28 -0600

    Add files via upload

---
+++ HW2/main 6.17.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Tobenna Nwufo
+2054054
+CISC 2348
+
+"""
+
+word = input()
+
+word = word.replace('i','!')
+word = word.replace('a','@')
+word = word.replace('m','M')
+word = word.replace('B','8')
+word = word.replace('o','.')
+
+word = word + 'q*s'
+
+print(word)
